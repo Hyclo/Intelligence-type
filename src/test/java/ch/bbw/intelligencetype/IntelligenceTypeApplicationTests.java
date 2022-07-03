36ec8f4695710c02d88f6faecce65416dd1853f3
Hyclo <miguel-aroldi@bluewin.ch>
2022-07-03 19:30:19 +0200

    init commit

---
+++ src/test/java/ch/bbw/intelligencetype/IntelligenceTypeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.intelligencetype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligenceTypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
